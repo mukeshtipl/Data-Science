2f5c0328962bd54ffcc6cea7890dbd1fb21f1b8d
MUKESH GUPTA <mugupta2@in.ibm.com>
2019-08-15 18:13:35 +0530

    deep_learning practice 15-Aug-2019

---
+++ deep_learning/pyimagesearch/Chapter09/gradient_descent.py
@@ -0,0 +1,111 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+
+# Method: Used to compute the sigmoid activation value
+def sigmoid_activation(x):
+    """
+    :param x: Feature matrix
+    :return: Predictions matrix
+    """
+    return 1.0 / (1 + np.exp(-x))
+
+
+# Method: Used to obtain a set of predictions
+def predict(x, w):
+    """
+    :param x: Feature matrix
+    :param w: Weights matrix
+    :return: Predictions matrix
+    """
+    # Take the dot product between the features and weight matrices to get the prediction matrix
+    predictions = sigmoid_activation(x.dot(w))
+
+    # Apply a step function threshold for the binary outputs
+    predictions[predictions <= 0.5] = 0
+    predictions[predictions > 0] = 1
+
+    return predictions
+
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-e', '--epochs', required=False, type=float, default=100,
+                help='# of epochs')
+ap.add_argument('-a', '--alpha', required=False, type=float, default=0.01,
+                help='learning rate')
+args = vars(ap.parse_args())
+
+# Generate a 2-class 2D classification problem with 1,000 data points
+(x, y) = make_blobs(n_samples=1000, n_features=2, centers=2, cluster_std=1.5, random_state=1)
+#print(x,y,x.shape,y.shape)
+y = y.reshape((y.shape[0]), 1)
+#print(x,y,x.shape,y.shape)
+
+
+# Insert a column of 1's as the last entry of the feature matrix (bias trick)
+x = np.c_[x, np.ones((x.shape[0]))]
+#print(x,y,x.shape,y.shape)
+# Split the data into training (50%) and testing (50%)
+(train_x, test_x, train_y, test_y) = train_test_split(x, y, test_size=0.5, random_state=42)
+
+# Initialize the weight matrix and list of losses
+print('[INFO]: Training....')
+w = np.random.randn(x.shape[1], 1)
+print("x,train_x.shape,w.shape:",x,train_x.shape,w.shape, w)
+
+losses = []
+
+# Loop for the number of epochs
+for epoch in np.arange(0, args['epochs']):
+    # Take the dot product between the features and weight matrices to get the predictions matrix
+    predictions = sigmoid_activation(train_x.dot(w))
+    print("p,y",predictions[1],train_y[1])
+    # Compute the error between the predictions and true values
+    error = predictions - train_y
+    loss = np.sum(error ** 2)
+    
+    losses.append(loss)
+
+    # Compute the gradient (dot product between the features and prediction errors)
+    gradient = train_x.T.dot(error)
+    print("grad",gradient)
+    print("gradient.shape",gradient.shape)
+
+    # Update the weight matrix by 'nudging' it in the negative direction
+    w += -args['alpha'] * gradient
+    print("new weight",w)
+
+    # Check to see if an update should be displayed
+    if epoch == 0 or (epoch+1) % 5 == 0:
+        print('[INFO]: epoch={}, loss={:.4f}'.format(int(epoch + 1), loss))
+        #print(predictions.shape,predictions[0:2])
+
+
+# Evaluate the model
+print('[INFO]: Evaluating....')
+predictions = predict(test_x, w)
+print(classification_report(test_y, predictions))
+
+# Plot the classification (test) data
+plt.style.use('ggplot')
+plt.figure()
+plt.title('Data')
+#plt.scatter(test_x[:, 0], test_x[:, 1], marker='o', c=test_y, s=30)
+plt.scatter(test_x[:, 0], test_x[:, 1], marker='o', c=test_y.reshape(test_y.shape[0]), s=30)
+#plt.scatter([1,2,3],[4,5,6],c=[0,1,0])
+
+print(test_y.reshape(test_y.shape[0]))
+
+# Plot the loss over time
+plt.style.use('ggplot')
+plt.figure()
+plt.title('Training Loss')
+plt.plot(np.arange(0, args['epochs']), losses)
+plt.xlabel('Epoch #')
+plt.ylabel('Loss')
+plt.show()
